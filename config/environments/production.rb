461e6d0eb3c2f4fa8c56bb3065c5049dc424e2ce
robindano <151894372+robindano@users.noreply.github.com>
2023-05-03 18:21:53 -0700

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
-  config.require_master_key = true
+  config.read_encrypted_secrets = true
 
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
